2de8f3738d1ea642147d57092866684a5e1f54f6
ttoomas <folprecht.to@gmail.com>
2022-03-04 21:55:09 +0100

    heroku pages path and connected to cleardb

--- app/database/connect.php
+++ app/database/connect.php
@@ -1,15 +1,19 @@
 <?php
 
-$host = 'localhost';
-$user = 'root';
-$password = '';
-$db_name = 'todo-app';
+// For development
+// $host = 'localhost';
+// $user = 'root';
+// $password = '';
+// $db_name = 'todo-app';
+
+// For Heroku pages
+$host = 'us-cdbr-east-05.cleardb.net';
+$user = 'ba8579d8c1871f';
+$password = '227db7cf';
+$db_name = 'heroku_c13cf00049fd9db';
 
 $conn = new MySQLi($host, $user, $password, $db_name);
 
 if($conn->connect_error){
     die("Database connection error: " . $conn->connect_error);
 }
-//else{
-//    echo "DB Connected successfully";
-//}
--- path.php
+++ path.php
@@ -2,4 +2,8 @@
 
 define("ROOT_PATH", realpath(dirname(__FILE__)));
 
-define("BASE_URL", "http://localhost/toDo-app");
+// For development
+// define("BASE_URL", "http://localhost/toDo-app");
+
+// For Heroku pages
+define("BASE_URL", "https://todo-app-ttoomas.herokuapp.com");
