a8e7c16c0350f46c1c12141a3b0ea7ce9654a476
ttoomas <folprecht.to@gmail.com>
2022-03-30 20:43:15 +0200

    heroku db connect fix

--- app/database/connect.php
+++ app/database/connect.php
@@ -16,7 +16,7 @@ $host = 'us-cdbr-east-05.cleardb.net';
 $user = 'ba8579d8c1871f';
 $db_name = 'heroku_c13cf00049fd9db';
 
-$conn = new MySQLi($host, $user, password, $db_name);
+$conn = new MySQLi($host, $user, dbPassword, $db_name);
 
 if($conn->connect_error){
     die("Database connection error: " . $conn->connect_error);
